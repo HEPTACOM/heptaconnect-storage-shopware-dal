116378fb858bb8e208e5190551491f79233e58a6
Joshua Behrens <behrens@heptacom.de>
2020-09-22 00:25:31 +0200

    Fix migration loading issue
    CONNECT-22

---
+++ src/MigrationSource.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Storage\ShopwareDal;
+
+use Shopware\Core\Framework\Migration\MigrationSource as ShopwareMigrationSource;
+
+class MigrationSource extends ShopwareMigrationSource
+{
+    public function __construct()
+    {
+        parent::__construct('HeptaConnectStorage', [
+            __DIR__.'/Migration' => 'Heptacom\HeptaConnect\Storage\ShopwareDal\Migration',
+        ]);
+    }
+}
