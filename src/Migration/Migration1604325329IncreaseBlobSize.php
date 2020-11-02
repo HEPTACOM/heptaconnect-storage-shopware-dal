e84866b2c83bfb65a1e8289570300f3abda47da8
Julian Krzefski <krzefski@heptacom.de>
2020-11-02 15:50:05 +0100

    Increase size for portal node storage from blob to longblob

---
+++ src/Migration/Migration1604325329IncreaseBlobSize.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Storage\ShopwareDal\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class Migration1604325329IncreaseBlobSize extends MigrationStep
+{
+    public const UP = <<<'SQL'
+alter table heptaconnect_portal_node_storage modify value longblob not null;
+SQL;
+
+    public function getCreationTimestamp(): int
+    {
+        return 1604325329;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $connection->exec(self::UP);
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+    }
+}
