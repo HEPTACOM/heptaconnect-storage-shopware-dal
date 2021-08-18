d30558bc082fe89ceeb6d069a6ca8c2c841f4fb9
Joshua Behrens <behrens@heptacom.de>
2021-08-18 09:10:13 +0200

    Add test support for shopware 6.4

--- test/Fixture/ShopwareKernel.php
+++ test/Fixture/ShopwareKernel.php
@@ -5,6 +5,8 @@ namespace Heptacom\HeptaConnect\Storage\ShopwareDal\Test\Fixture;
 
 use Shopware\Core\Framework\Plugin\KernelPluginLoader\StaticKernelPluginLoader;
 use Shopware\Core\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class ShopwareKernel extends Kernel
 {
@@ -22,5 +24,16 @@ class ShopwareKernel extends Kernel
             null,
             __DIR__.'/ShopwareProject'
         );
+    }
+
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
+    {
+        parent::configureContainer($container, $loader);
+        // Enables CSRF to fix 'The service "Shopware\Storefront\Framework\Csrf\CsrfPlaceholderHandler" has a dependency on a non-existent service "security.csrf.token_manager".'
+        $container->prependExtensionConfig('framework', [
+            'csrf_protection' => [
+                'enabled' => true,
+            ],
+        ]);
     }
 }
