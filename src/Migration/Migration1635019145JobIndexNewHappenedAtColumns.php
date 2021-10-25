28ca256ed3f33dc54715da65f4ff060ed2952547
Joshua Behrens <behrens@heptacom.de>
2021-10-25 13:39:26 +0200

    Add indices to new happened at columns in heptaconnect_job

---
+++ src/Migration/Migration1635019145JobIndexNewHappenedAtColumns.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Heptacom\HeptaConnect\Storage\ShopwareDal\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class Migration1635019145JobIndexNewHappenedAtColumns extends MigrationStep
+{
+    private const INDEX = <<<'SQL'
+CREATE INDEX `dt_desc.__TABLE__.__COL__` ON `__TABLE__` (`__COL__` desc);
+SQL;
+
+    public function getCreationTimestamp(): int
+    {
+        return 1635019145;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $this->addDateTimeIndex($connection, 'heptaconnect_job', 'started_at');
+        $this->addDateTimeIndex($connection, 'heptaconnect_job', 'finished_at');
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+    }
+
+    private function executeSql(Connection $connection, string $sql): void
+    {
+        // doctrine/dbal 2 support
+        if (\method_exists($connection, 'executeStatement')) {
+            $connection->executeStatement($sql);
+        } else {
+            $connection->exec($sql);
+        }
+    }
+
+    private function addDateTimeIndex(Connection $connection, string $table, string $column): void
+    {
+        $this->executeSql($connection, \str_replace(['__TABLE__', '__COL__'], [$table, $column], self::INDEX));
+    }
+}
