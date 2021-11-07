d537c6e1447803364bd07171d10a59a9fe367f2a
Joshua Behrens <behrens@heptacom.de>
2021-11-07 13:23:51 +0100

    Improve query iterator by predicting that next iteration is not worth to request
    CONNECT-288

--- src/Support/Query/QueryIterator.php
+++ src/Support/Query/QueryIterator.php
@@ -21,14 +21,18 @@ class QueryIterator
 
     protected function doIterate(QueryBuilder $query, int $fetchMode, ?callable $pack = null): iterable
     {
+        $maxResults = $query->getMaxResults();
+
         do {
             $rows = $query->execute()->fetchAll($fetchMode) ?: [];
 
             if (\is_array($rows)) {
+                $rows = \array_values($rows);
+
                 yield from \is_callable($pack) ? \array_map($pack, $rows) : $rows;
             }
 
-            $query->setFirstResult(($query->getFirstResult() ?? 0) + $query->getMaxResults());
-        } while ($rows !== []);
+            $query->setFirstResult(($query->getFirstResult() ?? 0) + $maxResults);
+        } while ($rows !== [] && \count($rows) >= $maxResults);
     }
 }
