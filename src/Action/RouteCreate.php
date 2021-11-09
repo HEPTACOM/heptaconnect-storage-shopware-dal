3ac10eeacbc00250f9ada6e03519e00c6fa7bdd2
Joshua Behrens <behrens@heptacom.de>
2021-11-09 23:46:35 +0100

    Add writing capabilities in RouteCreate
    CONNECT-288

--- src/Action/RouteCreate.php
+++ src/Action/RouteCreate.php
@@ -13,6 +13,7 @@ use Heptacom\HeptaConnect\Storage\Base\Contract\RouteCreateResult;
 use Heptacom\HeptaConnect\Storage\Base\Contract\StorageKeyGeneratorContract;
 use Heptacom\HeptaConnect\Storage\Base\Exception\UnsupportedStorageKeyException;
 use Heptacom\HeptaConnect\Storage\ShopwareDal\EntityTypeAccessor;
+use Heptacom\HeptaConnect\Storage\ShopwareDal\RouteCapabilityAccessor;
 use Heptacom\HeptaConnect\Storage\ShopwareDal\StorageKey\PortalNodeStorageKey;
 use Heptacom\HeptaConnect\Storage\ShopwareDal\StorageKey\RouteStorageKey;
 use Shopware\Core\Defaults;
@@ -27,14 +28,18 @@ class RouteCreate implements RouteCreateActionInterface
 
     private EntityTypeAccessor $entityTypes;
 
+    private RouteCapabilityAccessor $routeCapabilities;
+
     public function __construct(
         Connection $connection,
         StorageKeyGeneratorContract $storageKeyGenerator,
-        EntityTypeAccessor $entityTypes
+        EntityTypeAccessor $entityTypes,
+        RouteCapabilityAccessor $routeCapabilities
     ) {
         $this->connection = $connection;
         $this->storageKeyGenerator = $storageKeyGenerator;
         $this->entityTypes = $entityTypes;
+        $this->routeCapabilities = $routeCapabilities;
     }
 
     public function create(RouteCreatePayloads $params): iterable
@@ -59,10 +64,12 @@ class RouteCreate implements RouteCreateActionInterface
                 'type' => $param->getEntityType(),
                 'sourceKey' => $sourceKey->getUuid(),
                 'targetKey' => $targetKey->getUuid(),
+                'capabilities' => \array_values($param->getCapabilities()),
             ];
         }
 
         $entityTypeIds = $this->entityTypes->getIdsForTypes(\array_column($payload, 'type'), Context::createDefaultContext());
+        $capabilityIds = $this->routeCapabilities->getIdsForNames(\array_merge([], ...\array_column($payload, 'capabilities')));
         $keys = $this->storageKeyGenerator->generateKeys(RouteKeyInterface::class, \count($payload));
 
         foreach ($keys as $key) {
@@ -88,6 +95,19 @@ class RouteCreate implements RouteCreateActionInterface
                 'target_id' => Types::BINARY,
                 'type_id' => Types::BINARY,
             ]);
+
+            foreach ($data['capabilities'] ?? [] as $capability) {
+                $capabilityId = $capabilityIds[$capability];
+
+                $this->connection->insert('heptaconnect_route_has_capability', [
+                    'route_id' => Uuid::fromHexToBytes($key->getUuid()),
+                    'route_capability_id' => Uuid::fromHexToBytes($capabilityId),
+                    'created_at' => \date_create()->format(Defaults::STORAGE_DATE_TIME_FORMAT),
+                ], [
+                    'route_id' => Types::BINARY,
+                    'route_capability_id' => Types::BINARY,
+                ]);
+            }
 
             yield new RouteCreateResult($key);
         }
