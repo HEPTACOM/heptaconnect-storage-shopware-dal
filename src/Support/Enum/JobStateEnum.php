4a2d48dbb22054193084f639c436e437bb967405
Julian Krzefski <krzefski@heptacom.de>
2021-12-12 01:14:37 +0100

    CONNECT-291: Add missing return types

--- src/Support/Enum/JobStateEnum.php
+++ src/Support/Enum/JobStateEnum.php
@@ -16,17 +16,17 @@ abstract class JobStateEnum
         return null;
     }
 
-    public static function started()
+    public static function started(): string
     {
         return self::$started ??= \hex2bin('ca5ef83ffd114913a81477efafa14272');
     }
 
-    public static function failed()
+    public static function failed(): string
     {
         return self::$failed ??= \hex2bin('28ff3666f0f746cf84770c1148600605');
     }
 
-    public static function finished()
+    public static function finished(): string
     {
         return self::$finished ??= \hex2bin('6575ad837c71416f887d0e516a1bd813');
     }
