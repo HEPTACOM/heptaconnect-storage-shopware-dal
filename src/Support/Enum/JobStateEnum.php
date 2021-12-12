f9d8411e40cf328b357b86f8357e1b750b80203b
Julian Krzefski <krzefski@heptacom.de>
2021-12-12 01:05:37 +0100

    CONNECT-291: Add enum for job states

---
+++ src/Support/Enum/JobStateEnum.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Storage\ShopwareDal\Support\Enum;
+
+abstract class JobStateEnum
+{
+    private static ?string $started = null;
+
+    private static ?string $failed = null;
+
+    private static ?string $finished = null;
+
+    public static function open(): ?string
+    {
+        return null;
+    }
+
+    public static function started()
+    {
+        return self::$started ??= \hex2bin('ca5ef83ffd114913a81477efafa14272');
+    }
+
+    public static function failed()
+    {
+        return self::$failed ??= \hex2bin('28ff3666f0f746cf84770c1148600605');
+    }
+
+    public static function finished()
+    {
+        return self::$finished ??= \hex2bin('6575ad837c71416f887d0e516a1bd813');
+    }
+}
