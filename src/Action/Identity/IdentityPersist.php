65aa4daab4dfaae50adbfbcee010e472684ce046
Joshua Behrens <behrens@heptacom.de>
2022-02-20 00:21:53 +0100

    Fix persist test
    CONNECT-126

--- src/Action/Identity/IdentityPersist.php
+++ src/Action/Identity/IdentityPersist.php
@@ -518,8 +518,8 @@ class IdentityPersist implements IdentityPersistActionInterface
         $changedMappings = [];
 
         foreach ($update as $operation) {
-            $mappingNodeId = $operation['mappingNodeId'];
-            $externalId = $operation['externalId'];
+            $mappingNodeId = $operation['mapping_node_id'];
+            $externalId = $operation['external_id'];
 
             $changedMappings[$mappingNodeId][$externalId] = true;
         }
@@ -532,7 +532,7 @@ class IdentityPersist implements IdentityPersistActionInterface
         $deletedMappings = [];
 
         foreach ($delete as $operation) {
-            $mappingNodeId = $operation['mappingNodeId'];
+            $mappingNodeId = $operation['mapping_node_id'];
 
             $deletedMappings[$mappingNodeId] = true;
         }
