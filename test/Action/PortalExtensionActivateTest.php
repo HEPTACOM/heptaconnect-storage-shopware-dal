688ca4da04bf51070ac8b3eeb3b10bda3884436d
Joshua Behrens <behrens@heptacom.de>
2022-02-28 22:13:53 +0100

    $ make cs-fix
    CONNECT-501

--- test/Action/PortalExtensionActivateTest.php
+++ test/Action/PortalExtensionActivateTest.php
@@ -7,7 +7,6 @@ namespace Heptacom\HeptaConnect\Storage\ShopwareDal\Test\Action;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Types\Types;
 use Heptacom\HeptaConnect\Storage\Base\Action\PortalExtension\Activate\PortalExtensionActivatePayload;
-use Heptacom\HeptaConnect\Storage\ShopwareDal\Action\PortalExtension\PortalExtensionActivate;
 use Heptacom\HeptaConnect\Storage\ShopwareDal\Bridge\StorageFacade;
 use Heptacom\HeptaConnect\Storage\ShopwareDal\StorageKey\PortalNodeStorageKey;
 use Heptacom\HeptaConnect\Storage\ShopwareDal\Test\Fixture\Portal\Portal;
