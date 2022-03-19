75db68130666dda0060508c10bc0326b8f38c6c1
Joshua Behrens <behrens@heptacom.de>
2022-03-19 18:00:26 +0100

    Fix key generation
    CONNECT-126

--- test/Action/IdentityPersistTest.php
+++ test/Action/IdentityPersistTest.php
@@ -9,6 +9,7 @@ use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\PortalContract;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\MappingKeyInterface;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\MappingNodeKeyInterface;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\PortalNodeKeyInterface;
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\MappingKeyCollection;
 use Heptacom\HeptaConnect\Portal\Base\StorageKey\MappingNodeKeyCollection;
 use Heptacom\HeptaConnect\Storage\Base\Action\Identity\Overview\IdentityOverviewCriteria;
 use Heptacom\HeptaConnect\Storage\Base\Action\Identity\Overview\IdentityOverviewResult;
@@ -424,7 +425,7 @@ class IdentityPersistTest extends TestCase
         MappingNodeStorageKey $mappingNodeKey,
         string $externalId
     ): void {
-        $key = (new MappingNodeKeyCollection($this->storageKeyGenerator->generateKeys(MappingKeyInterface::class, 1)))->first();
+        $key = (new MappingKeyCollection($this->storageKeyGenerator->generateKeys(MappingKeyInterface::class, 1)))->first();
 
         if (!$key instanceof MappingStorageKey) {
             throw new UnsupportedStorageKeyException(\get_class($key));
