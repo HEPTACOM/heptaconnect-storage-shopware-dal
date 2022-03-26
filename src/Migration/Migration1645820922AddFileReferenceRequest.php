cefec514fa449d6784f28750cc1fcc8b37165304
Julian Krzefski <krzefski@heptacom.de>
2022-03-26 13:51:54 +0100

    CONNECT-25: Refactor migration to add indexes to database table

--- src/Migration/Migration1645820922AddFileReferenceRequest.php
+++ src/Migration/Migration1645820922AddFileReferenceRequest.php
@@ -25,12 +25,10 @@ create table heptaconnect_file_reference_request
 ENGINE=InnoDB
 DEFAULT CHARSET='binary'
 COLLATE='binary';
+SQL;
 
-create index `dt_desc.heptaconnect_file_reference_request.created_at`
-    on heptaconnect_file_reference_request (created_at);
-
-create index `dt_desc.heptaconnect_file_reference_request.updated_at`
-    on heptaconnect_file_reference_request (updated_at);
+    private const INDEX = <<<'SQL'
+CREATE INDEX `dt_desc.__TABLE__.__COL__` ON `__TABLE__` (`__COL__` desc);
 SQL;
 
     public function getCreationTimestamp(): int
@@ -40,15 +38,27 @@ SQL;
 
     public function update(Connection $connection): void
     {
-        // doctrine/dbal 2 support
-        if (\method_exists($connection, 'executeStatement')) {
-            $connection->executeStatement(self::UP);
-        } else {
-            $connection->exec(self::UP);
-        }
+        $this->executeSql($connection, self::UP);
+        $this->addDateTimeIndex($connection, 'heptaconnect_file_reference_request', 'created_at');
+        $this->addDateTimeIndex($connection, 'heptaconnect_file_reference_request', 'updated_at');
     }
 
     public function updateDestructive(Connection $connection): void
     {
+    }
+
+    private function executeSql(Connection $connection, string $sql): void
+    {
+        // doctrine/dbal 2 support
+        if (\method_exists($connection, 'executeStatement')) {
+            $connection->executeStatement($sql);
+        } else {
+            $connection->exec($sql);
+        }
+    }
+
+    private function addDateTimeIndex(Connection $connection, string $table, string $column): void
+    {
+        $this->executeSql($connection, \str_replace(['__TABLE__', '__COL__'], [$table, $column], self::INDEX));
     }
 }
