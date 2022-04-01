5e32cb2e91d4d67b8f20b1717ec6b089040a4369
Joshua Behrens <behrens@heptacom.de>
2022-04-01 22:26:40 +0200

    Add missing deleted_at check
    CONNECT-194

--- src/PortalNodeAliasAccessor.php
+++ src/PortalNodeAliasAccessor.php
@@ -44,6 +44,7 @@ class PortalNodeAliasAccessor
                 ->addOrderBy('portal_node.id')
                 ->andWhere($builder->expr()->in('portal_node.id', ':ids'))
                 ->andWhere($builder->expr()->isNotNull('portal_node.alias'))
+                ->andWhere($builder->expr()->isNull('portal_node.deleted_at'))
                 ->setParameter('ids', Id::toBinaryList($nonMatchingIds), Connection::PARAM_STR_ARRAY);
 
             $aliasedIds = [];
