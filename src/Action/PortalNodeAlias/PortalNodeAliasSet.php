cd94261e2e027e8ed349e08e6acdd6ee3be97caf
Joshua Behrens <behrens@heptacom.de>
2022-04-01 21:45:25 +0200

    Set updated at when portal node alias is set
    CONNECT-194

--- src/Action/PortalNodeAlias/PortalNodeAliasSet.php
+++ src/Action/PortalNodeAlias/PortalNodeAliasSet.php
@@ -14,6 +14,7 @@ use Heptacom\HeptaConnect\Storage\Base\Exception\UnsupportedStorageKeyException;
 use Heptacom\HeptaConnect\Storage\Base\Exception\UpdateException;
 use Heptacom\HeptaConnect\Storage\ShopwareDal\PortalNodeAliasAccessor;
 use Heptacom\HeptaConnect\Storage\ShopwareDal\StorageKey\PortalNodeStorageKey;
+use Heptacom\HeptaConnect\Storage\ShopwareDal\Support\DateTime;
 use Heptacom\HeptaConnect\Storage\ShopwareDal\Support\Id;
 
 class PortalNodeAliasSet implements PortalNodeAliasSetActionInterface
@@ -65,10 +66,13 @@ class PortalNodeAliasSet implements PortalNodeAliasSetActionInterface
 
         try {
             $this->connection->transactional(function () use ($updates): void {
+                $now = DateTime::nowToStorage();
+
                 // TODO batch
                 foreach ($updates as $portalNodeId => $alias) {
                     $this->connection->update('heptaconnect_portal_node', [
                         'alias' => $alias,
+                        'updated_at' => $now,
                     ], [
                         'id' => Id::toBinary($portalNodeId),
                     ], [
